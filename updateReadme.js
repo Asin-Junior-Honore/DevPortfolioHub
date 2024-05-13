4828cb12fa7464c6b4bc276b687e00bce39f6c1e
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-05-13 08:54:30 +0100

    modified updateReadme.js

--- updateReadme.js
+++ updateReadme.js
@@ -1,30 +1,23 @@
 const fs = require("fs");
-
 try {
-  // Check if contributors.json file exists
   if (!fs.existsSync("contributors.json")) {
     throw new Error("contributors.json file not found.");
   }
 
-  // Read contributors.json file
   const contributorsData = JSON.parse(
     fs.readFileSync("contributors.json", "utf8")
   );
 
-  // Check if contributorsData is an array
   if (!Array.isArray(contributorsData)) {
     throw new Error("Contributors data is not an array.");
   }
 
-  // Sort contributors alphabetically by name
   contributorsData.sort((a, b) => a.name.localeCompare(b.name));
 
-  // Generate markdown for contributors
   const contributorsMarkdown = contributorsData
     .map((contributor) => `- [${contributor.name}](${contributor.portfolio})`)
     .join("\n");
 
-  // Read README.md file
   let readme = fs.readFileSync("README.md", "utf8");
 
   // Find <!-- CONTRIBUTORS_START --> and <!-- CONTRIBUTORS_END --> placeholders in README.md
@@ -38,7 +31,6 @@ try {
     `<!-- CONTRIBUTORS_START -->\n${contributorsMarkdown}\n<!-- CONTRIBUTORS_END -->`
   );
 
-  // Write updated README.md file
   fs.writeFileSync("README.md", readme, "utf8");
 
   console.log("Readme updated successfully!");
